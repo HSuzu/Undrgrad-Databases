aa24fe8d0aac52809fc9aab01a22dcd724a11dc5
Henry Suzukawa <henry.shinji@gmail.com>
2018-10-24 17:28:42 -0300

    Remove unecessary condition

--- Q8.sql
+++ Q8.sql
@@ -11,9 +11,8 @@ select d1.forename, d1.surname, p1.position, d2.forename, d2.surname, p2.positio
 from positions as p1, positions as p2, positions p3, driver as d1, driver as d2
 where p2.lap - p1.lap = 1
 	and p1.lap = p3.lap
-	and (p2.position = p1.position
-	or (p1.position >= p2.position
-	and p1.position <= p3.position))
+	and (p1.position >= p2.position
+	and p1.position <= p3.position)
 	and p2.driverid = p3.driverid
 	and p2.position < p3.position
 	and p2.driverid <> p1.driverid
